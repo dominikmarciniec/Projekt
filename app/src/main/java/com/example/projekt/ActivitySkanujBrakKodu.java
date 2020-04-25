85ebeb11d38a2aeb63f25ba992510cc27a869c05
domin <dominik.marciniec@gmail.com>
2020-04-25 13:21:28 +0200

    spr działania

---
+++ app/src/main/java/com/example/projekt/ActivitySkanujBrakKodu.java
@@ -0,0 +1,41 @@
+package com.example.projekt;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+
+public class ActivitySkanujBrakKodu extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_skanuj_brakkodu);
+        TextView komunikat =(TextView) findViewById(R.id.komunikat);
+        Intent intent =getIntent();
+
+
+        komunikat.setText("kod"+intent.getStringExtra("value"));
+
+    }
+    public void changeActivity(View view) {
+        String button_text;
+
+        button_text = ((Button)view).getText().toString();
+
+        if(button_text.equals("Powtórz skanowanie")){
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+
+        }else if(button_text.equals("Dodaj produkt")){
+            Intent intent = new Intent(this, ActivityDodaj.class);
+            startActivity(intent);
+
+        }
+
+    }
+}
